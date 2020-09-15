d166488c9aaa16715409e9483bc29117b1be9a35
Miguel Angel Gomez Perez <mangel1098@outlook.com>
2020-09-14 23:23:42 -0500

    recibir mensaje socket

---
+++ lib/src/services/socket_service.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus {
+  Online,
+  Offline,
+  Connecting
+}
+
+class SocketService with ChangeNotifier {
+
+  ServerStatus _serverStatus = ServerStatus.Connecting;
+
+  get serverStatus => this._serverStatus;
+
+  SocketService() {
+    _initConfig();
+  }
+
+  void _initConfig() {
+    // Dart client
+    IO.Socket socket = IO.io('http://192.168.0.8:3000', {
+      'transports'  : ['websocket'],
+      'autoConnect' : true
+    });
+
+    socket.on('connect', (_) {
+     print('connect');
+     this._serverStatus = ServerStatus.Online;
+     notifyListeners(); 
+    });
+
+    socket.on('disconnect', (_) {
+      this._serverStatus = ServerStatus.Offline;
+     notifyListeners(); 
+    });
+
+    socket.on('nuevo-mensaje', (payload) {
+      print('nuevo-mensaje: $payload');
+      print('Nombre:' + payload['name']);
+      print('Mensaje:' + payload['mensaje']);
+      print(payload.containsKey('titulo')? payload['titulo'] : 'No hay titulo');
+    });
+  }
+}
